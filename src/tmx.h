fc6daf0e8d26db443b27b5f0d3324c716499b791
Arves100 <arves100@gmail.com>
2018-11-20 18:19:23 +0100

    Renamed template for msvc

--- src/tmx.h
+++ src/tmx.h
@@ -186,7 +186,7 @@ struct _tmx_obj { /* <object> */
 	double rotation;
 
 	char *name, *type;
-	tmx_template *template;
+	tmx_template *template_ref;
 	tmx_properties *properties;
 	tmx_object *next;
 };
