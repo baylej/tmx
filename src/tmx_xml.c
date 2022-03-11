06c9ba5512621d667462c83cca901f7a2a00a117
baylej <baylej@mrhide.eu>
2022-03-11 22:06:15 +0100

    layer.id no longer mandatory

--- src/tmx_xml.c
+++ src/tmx_xml.c
@@ -487,9 +487,6 @@ static int parse_layer(xmlTextReaderPtr reader, tmx_layer **layer_headadr, int m
 	if ((value = (char*)xmlTextReaderGetAttribute(reader, (xmlChar*)"id"))) { /* id */
 		res->id = atoi(value);
 		tmx_free_func(value);
-	} else {
-		tmx_err(E_MISSEL, "xml parser: missing 'id' attribute in the 'layer' element");
-		return 0;
 	}
 
 	if ((value = (char*)xmlTextReaderGetAttribute(reader, (xmlChar*)"name"))) { /* name */
